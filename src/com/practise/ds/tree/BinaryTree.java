77960900e124ebc382313591379de64dcb9d8bd1
Ganeshan Nagarajan <ganeshancse06@gmail.com>
2021-07-10 23:05:58 +0530

    AVL Tree added

--- src/com/practise/ds/tree/BinaryTree.java
+++ src/com/practise/ds/tree/BinaryTree.java
@@ -32,11 +32,62 @@ public class BinaryTree {
         return root;
     }
 
+    public boolean find(int value) {
+        TreeNode node = root;
+        while (node != null) {
+            if (value == node.value) {
+                return true;
+            }
+            if (value < node.value) {
+                node = node.left;
+            } else if (value > node.value) {
+                node = node.right;
+            }
+        }
+        return false;
+    }
+
+    public TreeNode deleteInTree(TreeNode root, int value){
+       if(root == null) {
+           return null;
+       }
+
+       if(value <root.value){
+           root.left = deleteInTree(root.left, value);
+       } else if (value > root.value){
+           root.right = deleteInTree(root.right, value);
+       }
+       
+       else {
+           if(root.left == null){
+               return root.right;
+           }
+           if(root.right == null){
+               return root.left;
+           }
+           root.value = getLeftSubTreeMaxValue(root.left);
+           root.left = deleteInTree(root.left, root.value);
+       }
+       return root;
+    }
+
+
+    private int getLeftSubTreeMaxValue(TreeNode root) {
+        int max = 0;
+        while (root != null) {
+            if (max < root.value) {
+                max = root.value;
+            }
+            root = root.right;
+        }
+        return max;
+    }
+
     /****************************************************************************************/
     // Traversal
 
-    public void preOrderTraversal(TreeNode root){
-        if(root == null){
+    public void preOrderTraversal(TreeNode root) {
+        if (root == null) {
             return;
         }
         preOrderTraversal(root.left);
@@ -44,16 +95,46 @@ public class BinaryTree {
         System.out.println(root.value);
     }
 
+    public void inOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrderTraversal(root.left);
+        System.out.println(root.value);
+        inOrderTraversal(root.right);
+    }
+
+    public void postOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.println(root.value);
+    }
 
     /****************************************************************************************/
 
     // Wrapper class
 
     public void add(int value) {
-        addInTree(root, value);
+        root = addInTree(root, value);
     }
+
+    public void delete(int value) {
+        root = deleteInTree(root, value);
+    }
+
     private void preOrder() {
         preOrderTraversal(root);
+    }
+
+    private void inOrder() {
+        inOrderTraversal(root);
+    }
+
+    private void postOrder() {
+        postOrderTraversal(root);
     }
 
     public static void main(String[] args) {
