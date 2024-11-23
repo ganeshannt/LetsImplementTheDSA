ba4b16218bb1aebea6448054ac9dbe124654b24a
Ganeshan N <ganeshancse06@gmail.com>
2024-11-23 17:28:32 +0530

    gitignore updated

---
+++ src/main/java/com/practise/lld/riderapp/Person.java
@@ -0,0 +1,8 @@
+package com.practise.lld.riderapp;
+/**
+ * @author Ganeshan Nagarajan
+ * @since 23/11/24
+ */
+
+public class Person {
+}
---
+++ src/main/java/com/practise/lld/riderapp/RideStatus.java
@@ -0,0 +1,2 @@
+package com.practise.lld.riderapp;public enum RideStatus {
+}
